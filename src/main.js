c922da98239b560b940d8321832bf346b756510c
Chinmay <signups@chinmay.design>
2024-02-12 23:17:18 +0100

    API is working

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import './styles/style.css'
+import axios from 'axios'
 
-console.log('Testing again')
+const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY
+const city = 'Manchester'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+const fetchWeather = async () => {
+  try {
+    const response = await axios.get(url)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchWeather()
